1ee712c5a13f06bada63563b05a2fa463e8b7749
kirthikaMurugadass <kirthika@maxappzrh.com>
2026-02-12 16:54:57 +0530

    /de changes

---
+++ dist/static/sanity-LC6H5E3s.js
@@ -0,0 +1,1 @@
+import{r as t,s as e}from"./sanity.config-BzMH2Zff.js";t(document.getElementById("sanity"),e,{reactStrictMode:!1,basePath:"/"});
--- i18n/routing.ts
+++ i18n/routing.ts
@@ -1,7 +1,8 @@
 import { defineRouting } from "next-intl/routing";
 
 export const routing = defineRouting({
-  locales: ["en", "de"],
-  defaultLocale: "en",
+  locales: ["de", "en"],
+  defaultLocale: "de",
   localePrefix: "always",
+  localeDetection: false,
 });
